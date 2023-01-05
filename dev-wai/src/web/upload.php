e24733d99e785197741c1e21dda082e36eb359b6
przeros <s180150@student.pg.edu.pl>
2023-01-05 22:06:40 +0100

    first commit

---
+++ dev-wai/src/web/upload.php
@@ -0,0 +1,18 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 1000000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" ) {
+  echo "Sorry, only JPG & PNG files are allowed.";
+  $uploadOk = 0;
+}
+?>
